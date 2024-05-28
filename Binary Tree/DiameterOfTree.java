631419cf272afdc3a13106eca9863479d8b96b37
Rohit Goswami <goswamirohit825@mail.com>
2024-05-28 14:29:01 +0530

    BInary Tree

--- Binary Tree/DiameterOfTree.java
+++ Binary Tree/DiameterOfTree.java
@@ -12,16 +12,16 @@ public class DiameterOfTree {
     }
   }
 
-  public static int height(Node root){
-    if(root==null){
-      return 0;
-    }
+  // public static int height(Node root){
+  //   if(root==null){
+  //     return 0;
+  //   }
 
-    int leftHeight = height(root.left);
-    int rightHeight = height(root.right);
+  //   int leftHeight = height(root.left);
+  //   int rightHeight = height(root.right);
 
-    return Math.max(leftHeight,rightHeight)+1;
-  }
+  //   return Math.max(leftHeight,rightHeight)+1;
+  // }
 
   // public static int diameter(Node root){ //Approach 1 -> O(N^2)
   //     if(root==null){
@@ -40,12 +40,26 @@ public class DiameterOfTree {
   //     return Math.max(max,selfDia);
   // }
 
+  static class Info{
+    int diam;
+    int ht;
 
-  public static int diameter(Node root){
-    if(root ==  null){
-      return 0;
+    Info(int diam, int ht){
+      this.diam = diam;
+      this.ht = ht;
     }
+  }
+  public static Info diameter(Node root){ //Approach 2-> O(n)
+    if(root ==  null){
+      return new Info(0,0);
+    }
+    Info leftInfo = diameter(root.left);
+    Info rightInfo = diameter(root.right);
 
+    int diam = Math.max(Math.max(leftInfo.diam,rightInfo.diam),leftInfo.ht+rightInfo.ht+1);
+    int ht = Math.max(leftInfo.ht,rightInfo.ht)+1;
+
+    return new Info(diam,ht);
     
   }
 
@@ -56,10 +70,10 @@ public class DiameterOfTree {
     root.right = new Node(3);
     root.left.left = new Node(4);
     root.left.right = new Node(5);
-    // root.right.left = new Node(6);
-    // root.right.right = new Node(7);
+    root.right.left = new Node(6);
+    root.right.right = new Node(7);
 
-    System.out.println("Diameter of tree: "+ diameter(root));
+    System.out.println("Diameter of tree: "+ diameter(root).diam);
 
   }
 }
--- Binary Tree/leetcode.java
+++ Binary Tree/leetcode.java
@@ -1,34 +1,46 @@
 
 import java.util.*;
 public class leetcode {
-  public static int minimumAddedInteger(int[] nums1, int[] nums2) {
-    int n = nums1.length;
-    int x = nums2.length;
-    Arrays.sort(nums1);
-    Arrays.sort(nums2);
-    int new_arr[] = new int[n-2];
-    for(int i = 0,k = 2; i < n-2; i++,k++){
-        new_arr[i] = nums1[k];
-    }
-    int minu = Integer.MAX_VALUE;
-    for(int i = 0; i < x; i++){
-        int tempu = nums2[i]-new_arr[i];
-        if(tempu+nums2[i]==nums1[i]){
-          minu = Math.min(minu,tempu);
-        }else{
-          continue;
-        }
+//   public static int minimumAddedInteger(int[] nums1, int[] nums2) {
+//     int n = nums1.length;
+//     int x = nums2.length;
+//     Arrays.sort(nums1);
+//     Arrays.sort(nums2);
+//     int new_arr[] = new int[n-2];
+//     for(int i = 0,k = 2; i < n-2; i++,k++){
+//         new_arr[i] = nums1[k];
+//     }
+//     int minu = Integer.MAX_VALUE;
+//     for(int i = 0; i < x; i++){
+//         int tempu = nums2[i]-new_arr[i];
+//         if(tempu+nums2[i]==nums1[i]){
+//           minu = Math.min(minu,tempu);
+//         }else{
+//           continue;
+//         }
 
         
-    }
-    return minu;
+//     }
+//     return minu;
+// }
+
+public static void printKClosest(int[] arr, int n, int k, int x) {
+  // code here
+  PriorityQueue<Integer> pq = new PriorityQueue<>();
+  for(int i:arr){
+      pq.add(i);
+  }
+  
+  while(!pq.isEmpty()){
+    System.out.println(pq.poll());
+  }
 }
 
 public static void main(String args[]){
-  int nums1[]  = {4,6,3,1,4,2,10,9,5};
-  int nums2[] = {5,10,3,2,6,1,9};
+  int nums[]  = {1,2,3,4,5,6,7};
+  // int nums2[] = {5,10,3,2,6,1,9};
 
-  System.out.println(minimumAddedInteger(nums1, nums2));
+  printKClosest(nums,0,0,0);
 
 }
 }
